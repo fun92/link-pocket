f0456a0c974f41cc164019432437f1e446607100
fun92 <fun4392@gmail.com>
2026-08-13 09:13:29 +0900

    Fix overlapping mobile auth screens

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = 'link-pocket-shell-v1';
-const SHELL = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icons/icon-180.png', '/icons/icon-192.png', '/icons/icon-512.png'];
+const CACHE = 'link-pocket-shell-v2';
+const SHELL = ['/', '/index.html', '/styles.css', '/mobile-fixes.css', '/app.js', '/manifest.webmanifest', '/icons/icon-180.png', '/icons/icon-192.png', '/icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
